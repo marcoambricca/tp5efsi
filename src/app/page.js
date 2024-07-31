e22042427beae220281636d25a0d4f2d234e2569
Android Studio <devandroid@ort>
2024-07-31 14:33:26 -0300

    fetch not working

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,26 @@
+'use client'
+
+import { useState, useEffect } from "react"
+
 export default function Home() {
-  
+  const [flags, setFlags] = useState([]);
+
+  const fetchFlags = async () => {
+    const response = await fetch('https://countriesnow.space/api/v0.1/countries/flag/images');
+    const data = await response.json();
+    return data;
+  }
+
+  useEffect(() => {
+    fetchFlags(setFlags);
+    console.log(flags);
+  }, []);
+
+  console.log(flags);
+
+  return(
+    <div className="game">
+      
+    </div>
+  )
 }
