9a1ba76b25bc4a0a473100d14e21da3eeb542d0e
MARCO AMBRICCA <47490408@ort>
2024-08-01 12:11:11 -0300

    card css

--- src/app/components/flag-card.jsx
+++ src/app/components/flag-card.jsx
@@ -1,13 +1,14 @@
 import Image from "next/image"
 
 export default function FlagCard(obj){
+    console.log(obj)
     return(
         <div className="flag-card">
             <Image
                 src={obj.flag}
                 width={700}
                 height={500}
-                alt={obj.name + ' flag'}
+                alt={obj.name}
             />
             <h1 className="country-name">{obj.name}</h1>
         </div>
